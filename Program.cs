f6eb167afa43ba3fc8d8d70a53db03c0e9f8ae5b
ionut <ionutcatana1805@gmail.com>
2023-10-27 15:33:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Multiplication
+            int multi = 18 * 50;
+            Console.WriteLine(multi);
+            Console.ReadLine();
+
+            // Addition
+            int add = 18 + 25;
+            Console.WriteLine(add);
+            Console.ReadLine();
+
+            // Division
+            double div = 18.0 / 12.5;
+            Console.WriteLine(div);
+            Console.ReadLine();
+
+            // Comparison
+            bool comp = 18 > 50;
+            Console.WriteLine(comp);
+            Console.ReadLine();
+
+            // Remainder
+            int remdr = 18 % 7;
+            Console.WriteLine(remdr);
+            Console.ReadLine();
+            
+        }
+    }
+}
